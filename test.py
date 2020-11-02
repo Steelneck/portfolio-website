79cd85ae0a5273e088722ede13ea8c7368661971
Joel Stålnacke <joest466@student.liu.se>
2020-11-02 15:00:54 +0100

    changed name of files

---
+++ test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  2 14:43:58 2020
+
+@author: Jolle
+"""
+def printf():
+    print("File initiated")
+    
+
+def init():
+    """Test"""
+    printf()
---
+++ test2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  2 14:48:54 2020
+
+@author: Jolle
+"""
+
+import test
+
+init()
