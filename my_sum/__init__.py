46f93dd822b2742dfad184845c924cbd2221bda0
Joel Stålnacke <joest466@student.liu.se>
2020-11-02 16:17:22 +0100

    la till testfalls exempel

---
+++ my_sum/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  2 15:34:56 2020
+
+@author: Jolle
+"""
+
+def sum(arg):
+    total = 0
+    for val in arg:
+        total += val
+    return total
---
+++ testcase.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  2 15:35:56 2020
+
+@author: Jolle
+"""
+
+import unittest
+from fractions import Fraction
+
+from my_sum import sum
+
+
+class TestSum(unittest.TestCase):
+    def test_list_int(self):
+        """
+        Test that it can sum a list of integers
+        """
+        data = [1, 2, 3]
+        result = sum(data)
+        self.assertEqual(result, 6)
+        
+    def test_list_fraction(self):
+        """
+        Test that it can sum a list of fractions
+        """
+        data = [Fraction(1, 4), Fraction(1, 4), Fraction(2, 5)]
+        result = sum(data)
+        self.assertEqual(result, 1)
+
+if __name__ == '__main__':
+    unittest.main()
